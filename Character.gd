daa9e4f8c44416686c211e3089bfd0c30eb9ad3c
Andrew C Wrenn <awrenn53@gmail.com>
2020-11-02 07:57:04 -0800

    Character can at least walk

---
+++ Character.gd
@@ -0,0 +1,35 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+export var speed = 400  # How fast the player will move (pixels/sec).
+var screen_size  # Size of the game window.
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2()  # The player's movement vector.
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$Pivot/Body.animation = "walk"
+	else:
+		$Pivot/Body.animation = "idle"
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+	$Pivot/Body.flip_v = false
+	$Pivot/Body.flip_h = velocity.x < 0
+
+
