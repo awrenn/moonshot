a8d4869179872b87f41c2022afcdaf02188161a8
Andrew C Wrenn <awrenn53@gmail.com>
2020-11-02 16:48:55 -0800

    Now he runs in a circle

--- Character.gd
+++ Character.gd
@@ -1,17 +1,20 @@
 extends KinematicBody2D
 
 
-export var alpha = PI  # How fast the player will move (rads/sec).
+export var alpha = PI 
+export var jump_speed = 100 # How fast the player will move (rads/sec).
 var screen_size  # Size of the game window.
 var theta = 0.0 # Degrees rotation - 0 is top middle
 var r = 50 # Distance from center 
 var floor_r = 50
-var center_x = 50.0
-var center_y = 50.0
+var center_x
+var center_y
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
+	center_x = screen_size.x / 2
+	center_y = screen_size.y / 2
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -33,7 +36,7 @@ func _process(delta):
 		
 
 	theta += alpha * delta * velocity.x
-	r += alpha * delta * velocity.y
+	r += jump_speed * delta * velocity.y
 	r = clamp(r, floor_r, r)
 	position.x = center_x + r * cos(theta)
 	position.y = center_y + r * sin(theta)
