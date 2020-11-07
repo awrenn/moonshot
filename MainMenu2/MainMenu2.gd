68d6a69dee8ef70311b08bbaaf4b400009e1cd88
Andrew C Wrenn <awrenn53@gmail.com>
2020-11-07 09:54:41 -0800

    Start at the main menu, and open World scene when new game is pressed

--- MainMenu2/MainMenu2.gd
+++ MainMenu2/MainMenu2.gd
@@ -2,9 +2,11 @@ extends CanvasLayer
 
 export (float) var speed = 25
 var nodes = []
+var Scenes = ["res://World.tscn", "", ""]
 var idx = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$Forest.transform.scaled(Vector2(1.7, 4))
 	$CenterContainer/Camera2D.position.y = get_viewport().size.y / 2
 	nodes.append($CenterContainer/Menu/MenuItem/Sprite)
 	nodes.append($CenterContainer/Menu/MenuItem2/Sprite)
@@ -25,7 +27,7 @@ func get_input():
 		
 	if Input.is_action_just_pressed("ui_accept"):
 		# TODO swith to the new scene
-		print("SWITCH SCENES HERE")
+		get_tree().change_scene(Scenes[idx])
 
 func _process(delta):
 	get_input()
